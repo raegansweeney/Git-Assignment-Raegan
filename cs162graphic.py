c182e317bfdfd945f6609f4f0df46dd824cf3a8b
raegansweeney <129887785+raegansweeney@users.noreply.github.com>
2023-04-11 11:29:50 -0700

    Add files via upload

---
+++ cs162graphic.py
@@ -0,0 +1,17 @@
+# Raegan Sweeney
+# Computer Science 162
+# 4/11/2023
+
+# copied code for assignment setup ---------------------------------------------------------#
+import matplotlib.pyplot as plot
+# set up your lists
+numlist = [8, 6, 5, 3]
+namelist = ['Freshmen', 'Sophomores', 'Juniors', 'Seniors']
+colorlist = ['pink', 'red', 'orange', 'yellow' ]
+explodelist = [0.1, 0.0, 0.0, 0.0]
+# make the pie chart
+plot.pie(numlist, labels=namelist, autopct='%1.1f%%', colors=colorlist, explode=explodelist, startangle = 90)
+plot.axis('equal')
+plot.savefig('piechart.png')
+# ------------------------------------------------------------------------------------------#
+
